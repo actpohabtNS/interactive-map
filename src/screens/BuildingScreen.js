40673bcc9f60ef15529b7f06be558f1fe964e68f
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-03-30 22:06:52 +0300

    [ ADDED ] select floor block

--- src/screens/BuildingScreen.js
+++ src/screens/BuildingScreen.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Header from '../components/Header'
+import Map from '../components/Map'
 
 const BuildingScreen = () => {
   return (
-    <div>
-      <Header
-        backText="Обрати поверх"
-      >
+    <div className="building-screen">
+      <Header>
         Факультет комп’ютерних наук та кібернетики
       </Header>
+
+      <span className="sf-text">Оберіть поверх</span>
+
+      <div className="sf-block">
+        <Map className="sf-map"/>
+        <div className="sf-floors-list">
+
+        </div>
+      </div>
     </div>
   )
 }
