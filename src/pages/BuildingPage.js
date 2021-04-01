2a6ce3d5f8cdfd7703a3bb6f5f08a71ad773a52c
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-04-01 12:13:39 +0300

    [ REFACTORED ] data-driven page generation

--- src/pages/BuildingPage.js
+++ src/pages/BuildingPage.js
@@ -3,6 +3,8 @@ import Header from '../components/Header'
 import Map from '../components/Map'
 import FloorItem from '../components/FloorItem'
 
+const floorsData = require('../data/floors.json')
+
 const BuildingPage = () => {
   return (
     <div className="building-screen">
@@ -15,30 +17,7 @@ const BuildingPage = () => {
       <div className="sf-block">
         <Map className="sf-map"/>
         <div className="sf-floors-list">
-          <FloorItem
-            floorNum={5}
-            floorText="Тут знаходяться не дуже важливі кімнати, такі як комори, кабінети, комп’ютери."
-          />
-
-          <FloorItem
-            floorNum={4}
-            floorText="Цей поверх призначено більше для студентів, ніж викладачів. Тут можна знайти буфет, їдальну та навіть декілька спальних кімнат."
-          />
-
-          <FloorItem
-            floorNum={3}
-            floorText="Найважливіший поверх для навчання студентів. Саме тут ви можете побачити більшість кабінетів з технікою, людей, відповідальних за неї тощо."
-          />
-
-          <FloorItem
-            floorNum={2}
-            floorText="Адміністративний поверх факультету, тут знаходяться обидва деканати, кабінет декана, всіма улюблена Читалка, звичайна бібліотека та проходи на інші факультети."
-          />
-
-          <FloorItem
-            floorNum={1}
-            floorText="Великий хол для різних подій та виставок, роздягальня, баки для сортування сміття, навчальні кабінети, канцелярні товари та кафе."
-          />
+          { floorsData.map(floor => <FloorItem floorNum={ floor.num } floorText={ floor.description } />) }
         </div>
       </div>
     </div>
